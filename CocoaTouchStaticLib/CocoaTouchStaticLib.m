4360fc66ef36de9b8f85a9465b72e1d65017fac2
richard <xsailor511@163.com>
2016-10-08 12:53:08 +0800

    Initial Commit

---
+++ CocoaTouchStaticLib/CocoaTouchStaticLib.m
@@ -0,0 +1,13 @@
+//
+//  CocoaTouchStaticLib.m
+//  CocoaTouchStaticLib
+//
+//  Created by richard on 2016/10/8.
+//  Copyright © 2016年 richard. All rights reserved.
+//
+
+#import "CocoaTouchStaticLib.h"
+
+@implementation CocoaTouchStaticLib
+
+@end
